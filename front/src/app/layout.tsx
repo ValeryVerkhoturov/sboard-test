befe5a7de3fb7a6fb1a3fd7bd2b289017696ef2c
ValeryVerkhoturov <verkhoturov.valery@icloud.com>
2023-07-14 09:33:20 +0300

    feat: add navbar

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,5 +1,9 @@
+'use client'
+
+import React, {useState} from 'react'
 import { Inter } from 'next/font/google'
 import StyledComponentsRegistry from './registry'
+import {Bar, Hamburger, Logo, MainNav, NavBarToggle, NavLi, NavLink} from "@/components/layout";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,10 +12,32 @@ export default function RootLayout({
 }: {
     children: React.ReactNode
 }) {
+
+    const [displayNav, setDisplayNav] = useState<'flex' | 'none'>('flex')
+
+    const toggleNavBar = () => setDisplayNav(displayNav === 'flex' ? 'none' : 'flex')
+
     return (
         <html lang="en">
             <body className={inter.className}>
                 <StyledComponentsRegistry>
+                    <Bar>
+                        <NavBarToggle onClick={toggleNavBar}>
+                            <Hamburger />
+                        </NavBarToggle>
+                        <Logo href="#">logo</Logo>
+                        <MainNav $display={displayNav}>
+                            <NavLi>
+                                <NavLink href="/posts" >Посты</NavLink>
+                            </NavLi>
+                            <NavLi>
+                                <NavLink href="/login" >Войти</NavLink>
+                            </NavLi>
+                            <NavLi>
+                                <NavLink href="/register" >Зарегистрироваться</NavLink>
+                            </NavLi>
+                        </MainNav>
+                    </Bar>
                     {children}
                 </StyledComponentsRegistry>
             </body>
